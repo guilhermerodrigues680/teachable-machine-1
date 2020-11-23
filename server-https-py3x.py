124116e195953e29afae4eb5a2660ec75cbb90a8
guilhermerodrigues680 <guilhermerodrigues680@gmail.com>
2020-11-23 20:53:47 -0300

    Adicionado servidor https python

---
+++ server-https-py3x.py
@@ -0,0 +1,15 @@
+# Python 3
+# taken from https://piware.de/2011/01/creating-an-https-server-in-python/
+# generate server.pem with the following command:
+#    openssl req -new -x509 -keyout server.pem -out server.pem -days 365 -nodes
+# run as follows:
+#    python3 server-https-py3x.py
+# then in your browser, visit:
+#    https://localhost:4443
+
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+import ssl
+
+httpd = HTTPServer(('localhost', 4443), SimpleHTTPRequestHandler)
+httpd.socket = ssl.wrap_socket(httpd.socket, certfile='server.pem', server_side=True)
+httpd.serve_forever()
